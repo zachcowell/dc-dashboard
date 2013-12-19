30f3bdd1fa46f44f8e479427091ce580fc0bef0e
zachcowell <zach.cowell@gmail.com>
2013-12-19 12:38:43 -0500

    socket.io integrated to twitter live stream

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,51 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MainCtrl', function ($scope, $routeParams, $http) {
-
-   $scope.chartConfig = {
-        options: { 
-          chart: { type: 'spline' },
-          legend: { enabled: true },
-          plotOptions: { spline: { marker: { enabled: false } } }, 
-        },
-          xAxis: { title: { enabled: true, text: 'Year' },
-          labels: { formatter: function () { return Highcharts.numberFormat(this.value, 0, '', ''); } } },
-          yAxis: { reversed: true, title: { text: 'Rank' }, min: 1, max: 900},              
-        title: { text: 'Popularity Over Time' },
-        series: [],
-        credits: { enabled: false },
-        loading: false
-    };
-
-    $scope.retrieveData = function(){
-      $http({
-        method: 'GET',
-        url: '/name/' + $scope.name
-      }).
-      success(function (data, status, headers, config) {
-        if (! data.length > 0) { alert('No results for ' + $scope.name); }
-        else {
-          var allRecords = Metrics.getRecordsByGender(data,$scope.years,$scope.gender);
-          
-          $scope.chartConfig.series.push({
-            name: data[0].name,
-            data: Metrics.getRankAndYearSeriesPair(allRecords)
-          });
-        }
-        
-      }).
-      error(function (data, status, headers, config) {
-        $scope.name = 'Error!'
+  controller('MainCtrl', function ($scope, socket, $routeParams, $http) {
+      $scope.stuff=[];
+      //console.log(socket);
+      socket.on('message',function(data){ 
+        //console.log(data);
+        $scope.stuff.push(data);
       });
-    };
-
-  }).
-  controller('MyCtrl1', function ($scope) {
-    // write Ctrl here
-
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
-
+      $scope.$on('socket:message', function (ev, data) {
+        console.log(data);
+     });
   });
