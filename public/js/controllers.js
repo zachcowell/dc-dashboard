ca4d5185a4d7f9d9be236ceb7d3ede483ce345fa
Zach Cowell <cowell_zach@bah.com>
2013-12-19 14:11:11 -0500

    able to pass full Tweet json object through socket.emit in lieu of socket.send

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,11 +6,11 @@ angular.module('myApp.controllers', []).
   controller('MainCtrl', function ($scope, socket, $routeParams, $http) {
       $scope.stuff=[];
       //console.log(socket);
-      socket.on('message',function(data){ 
-        //console.log(data);
-        $scope.stuff.push(data);
-      });
-      $scope.$on('socket:message', function (ev, data) {
+      socket.on('data',function(data,ev){ 
         console.log(data);
-     });
+        //$scope.stuff.push(ev);
+      });
+      //$scope.$on('socket:message', function (ev, data) {
+        //console.log(data);
+     //});
   });
