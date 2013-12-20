c44ce1bb210a2bcffb3122385087781cfc000bd8
Zach Cowell <cowell_zach@bah.com>
2013-12-19 22:10:41 -0500

    adding server again

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+	routes = require('./routes'),
+	socketIo = require('socket.io'),
+	http = require('http'),
+	path = require('path'),
+	_ = require('underscore'),
+	app = module.exports = express(),
+	server = http.createServer(app),
+	io = socketIo.listen(server,{ log: false }),
+	Twit = require('twit');
+
+var tweetStack = [];
+
+io.sockets.on('connection', function(socket) { 
+    _.each(tweetStack, function(t){ socket.emit('data',t); });
+});
+
+var TwitterWorker = function () {
+	var T = new Twit({ consumer_key:'D8g2hursTwtrTZlgiu7JwA', consumer_secret:'rPraYI7YgWPhrJJxSXtBHHXkJ0UyyooL0CxyRgCtw', access_token:'339061245-ZUr76Hf6QnLt6Vq8XzqGGIpACWDSurvuMiYDuccD', access_token_secret:'GG88Bjiwm378Xh5rk50VZNRiiYby6JcT0Rz8u2vB8'});
+	var DC = ['-77.222069','38.793786','-76.832489','39.030227']; //setup DC bounding box
+	var stream = T.stream('statuses/filter', { locations: DC })
+	stream.on('tweet', function (tweet) { 
+		io.sockets.emit('data',tweet); 
+		if (tweetStack.length > 132 ) tweetStack = tweetStack.splice(11);
+		tweetStack.push(tweet);
+	});
+}();
+
+app.configure(function(){
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(app.router);
+});
+
+if (app.get('env') === 'development') { app.use(express.errorHandler()); }
+if (app.get('env') === 'production') { };
+
+app.get('/', routes.index);
+//app.get('*', routes.index);
+
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
