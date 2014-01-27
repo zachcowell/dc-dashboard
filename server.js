d19dc5f4e6bcb4de78bffc72b052f8992c87e3e0
Zach Cowell <zach.cowell@gmail.com>
2014-01-27 00:12:56 -0500

    api updates

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express'),
 	routes = require('./routes'),
 	path = require('path'),
 	_ = require('underscore'),
+	mongoose = require('mongoose'),
 	app = module.exports = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server,{ log: false }),
 	Twit = require('twit'),
-	twitterworker = require('./routes/twitterworker');
+	twitterworker = require('./routes/twitterworker'),
+	twitterapi= require('./routes/twitterapi');
 
 var tweetStack = [];
 
@@ -30,8 +32,18 @@ app.configure(function(){
 });
 var env = app.get('env');
 
-if (env === 'development') { app.use(express.errorHandler()); }
-if (env === 'production') { };
+if (env === 'development') { 
+	app.use(express.errorHandler()); 
+	mongoose.connect('mongodb://localhost/tweety'); 
+}
+if (env === 'production') { 
+	mongoose.connect('mongodb://lowcrow:bravo@ds027519.mongolab.com:27519/tweety'); 
+};
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () { console.log('Database opened'); });
 
 var TwitterBlock = function(){
 	io.sockets.on('connection', function(socket) { _.each(tweetStack, function(t){ socket.emit('tweetmap',t); }); });
@@ -39,10 +51,18 @@ var TwitterBlock = function(){
 	var searchParams = { q: 'geocode:38.877831,-77.019256,0.2km', count: 100 };
 	var locations = {
 		DC : { locations: ['-77.222069','38.793786','-76.832489','39.030227'] },
+		london : { locations: ['-0.57','51.2591','0.3407','51.743'] },
+		moscow : { locations: ['37.3207','55.5167','37.9591','55.9944'] },
+		cairo : { locations: ['31.118881','29.967812','31.471658','30.179214'] },
 		planetEarth : { locations: ['-180','-90','180','90'] }
 	}
 	
-	var stream = twitterworker.getStream(env,locations.DC);
+	var stream = twitterworker.getStream(env,
+		{ locations : 
+			locations.london.locations
+			.concat(locations.moscow.locations)
+			.concat(locations.cairo.locations)
+		});
 	
 	var tweetMapping = function(socketName, preserveOnServer){
 		return function(tweet){
@@ -50,6 +70,7 @@ var TwitterBlock = function(){
 			if (preserveOnServer) {
 				if (tweetStack.length > 132 ) tweetStack = tweetStack.splice(11);
 				tweetStack.push(tweet);
+				twitterapi.insert(tweet);
 			}
 		}
 	}
@@ -60,6 +81,8 @@ var TwitterBlock = function(){
 app.get('/', routes.index);
 app.get('/api/local/:input',twitterworker.getUsersNearMe(env))
 app.get('/partials/:name', routes.partials);
+app.get('/api/list',twitterapi.list);
+app.get('/api/list/full',twitterapi.bigList);
 app.get('*', routes.index);
 
 server.listen(app.get('port'), function () {
