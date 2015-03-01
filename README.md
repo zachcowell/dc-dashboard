55a5819ad7897be9c8507efd1e63b61c8acdab7a
Zach Cowell <zach.cowell@gmail.com>
2015-03-01 00:11:14 -0500

    Removed codebase

--- README.md
+++
@@ -1,5 +0,0 @@
-dc-dashboard
-============
-
-Social dashboard for DC. 
-Uses Node.js + Twitter + Instagram + other API mashups to come.
--- routes/api.js
+++
@@ -1,8 +0,0 @@
-var Tweets = require('../models/tweetmodel.js');
-
-exports.list = function(req, res) {
-	Tweets.find({  }, function (err, data) {
-	  if (err) return handleError(err);
-	  res.send(data);
-	})
-}
