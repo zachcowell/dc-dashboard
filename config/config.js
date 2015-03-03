e8456d2dd78a049a36bf3ddc33a24984ce55da31
Zach Cowell <zach.cowell@gmail.com>
2015-03-02 20:03:33 -0500

    Rotating credentials and removing them from public view

--- config/config.js
+++ config/config.js
@@ -7,16 +7,16 @@ module.exports = {
 		  password : ''
 		},
 		'prod':{	
-		  host     : 'us-cdbr-iron-east-01.cleardb.net',
-		  database : 'heroku_8a6d4346d2b8beb',
-		  user     : 'bb367d14d59350',
-		  password : '248b7a23'
+		  host     : 'ADD_YOUR_OWN_HERE',
+		  database : 'ADD_YOUR_OWN_HERE',
+		  user     : 'ADD_YOUR_OWN_HERE',
+		  password : 'ADD_YOUR_OWN_HERE'
 		}
 	},
 	'twitter':{
-		consumer_key: "D8g2hursTwtrTZlgiu7JwA", 
-		consumer_secret: "rPraYI7YgWPhrJJxSXtBHHXkJ0UyyooL0CxyRgCtw", 
-		access_token: "339061245-ZUr76Hf6QnLt6Vq8XzqGGIpACWDSurvuMiYDuccD", 
-		access_token_secret: "GG88Bjiwm378Xh5rk50VZNRiiYby6JcT0Rz8u2vB8"
+		consumer_key: "ADD_YOUR_OWN_HERE", 
+		consumer_secret: "ADD_YOUR_OWN_HERE", 
+		access_token: "ADD_YOUR_OWN_HERE", 
+		access_token_secret: "ADD_YOUR_OWN_HERE"
 	}
 }
